28e12dce2d413e371a53b019f58aa6034d2e2dad
Kishan Kumar <iamkishanchandravanshi@gmail.com>
2026-05-17 19:29:32 +0530

    added application properties in gitignore

--- src/main/java/com/_xkishan/journal/service/JournalService.java
+++ src/main/java/com/_xkishan/journal/service/JournalService.java
@@ -3,6 +3,7 @@ package com._xkishan.journal.service;
 import com._xkishan.journal.entity.JournalEntry;
 import com._xkishan.journal.entity.UserEntry;
 import com._xkishan.journal.repository.JournalRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class JournalService {
 
@@ -23,14 +25,21 @@ public class JournalService {
     @Transactional
     public JournalEntry createJournal(JournalEntry journalEntry,
                                       String username) {
-        UserEntry user = userService.findByUserName(username);
 
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalRepository.save(journalEntry);
+        try {
+            UserEntry user = userService.findByUserName(username);
 
-        user.getJournalEntries().add(saved);
-        userService.save(user);
-        return saved;
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalRepository.save(journalEntry);
+
+            user.getJournalEntries().add(saved);
+            userService.save(user);
+            return saved;
+        } catch (Exception e) {
+            log.info("An error occurred while saving the journal ", e);
+            throw e;
+        }
+
     }
 
     public JournalEntry updateJournal(JournalEntry updatedEntry) {
